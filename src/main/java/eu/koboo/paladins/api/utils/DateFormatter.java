7664c71c014ed284ab5be9ed4ac7e942949b6f41
Koboo <admin@koboo.eu>
2022-10-23 18:02:49 +0200

    Update comments

--- src/main/java/eu/koboo/paladins/api/utils/DateFormatter.java
+++ src/main/java/eu/koboo/paladins/api/utils/DateFormatter.java
@@ -6,8 +6,11 @@ import java.util.TimeZone;
 
 public class DateFormatter {
 
+    // Used for the url timeStamp parameter
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss");
+    // Used by the api-endpoints for date-fields
     private static final SimpleDateFormat API_PARSE_DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss a");
+    // Used by the api-paths as parameter
     private static final SimpleDateFormat API_PATH_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
     static {
