4dff64a95930f11414ccebcc3284b42f5e8e32ec
Koboo <admin@koboo.eu>
2022-11-03 17:23:20 +0100

    Create multiple Date parsing

--- src/main/java/eu/koboo/paladins/api/utils/DateFormatter.java
+++ src/main/java/eu/koboo/paladins/api/utils/DateFormatter.java
@@ -8,15 +8,29 @@ public class DateFormatter {
 
     // Used for the url timeStamp parameter
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss");
+
     // Used by the api-endpoint "gethirezserverstatus"
     private static final SimpleDateFormat STATUS_PARSE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-    // Used by the api-endpoints for date-fields
-    private static final SimpleDateFormat API_PARSE_DATE_FORMAT = new SimpleDateFormat("M/d/yyyy HH:mm:ss a");
+
     // Used by the api-paths as parameter
     private static final SimpleDateFormat API_PATH_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
+    // Used by the api-endpoints for date-fields
+    private static final SimpleDateFormat[] API_RESPONSE_DATE_FORMATS = new SimpleDateFormat[]{
+            new SimpleDateFormat("M/d/yyyy HH:mm:ss a"),
+            new SimpleDateFormat("MM/dd/yyyy HH:mm:ss a"),
+            new SimpleDateFormat("MM/d/yyyy HH:mm:ss a"),
+            new SimpleDateFormat("M/dd/yyyy HH:mm:ss a"),
+    };
+
     static {
-        SIMPLE_DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
+        SIMPLE_DATE_FORMAT.setTimeZone(timeZone);
+        STATUS_PARSE_DATE_FORMAT.setTimeZone(timeZone);
+        API_PATH_DATE_FORMAT.setTimeZone(timeZone);
+        for (SimpleDateFormat dateFormat : API_RESPONSE_DATE_FORMATS) {
+            dateFormat.setTimeZone(timeZone);
+        }
     }
 
     public static String formatDate() {
@@ -24,11 +38,16 @@ public class DateFormatter {
     }
 
     public static long parseFrom(String string) {
-        try {
-            return API_PARSE_DATE_FORMAT.parse(string).getTime();
-        } catch (Exception e) {
-            throw new RuntimeException("Couldn't parse date from string \"" + string + "\": ", e);
+        for (SimpleDateFormat dateFormat : API_RESPONSE_DATE_FORMATS) {
+            try {
+                Date parsedDate = dateFormat.parse(string);
+                if(parsedDate == null) {
+                    continue;
+                }
+                return parsedDate.getTime();
+            } catch (Exception ignored) { }
         }
+        throw new RuntimeException("Couldn't find parseable date for string \"" + string + "\":");
     }
 
     public static long parseFromServerStatus(String string) {
